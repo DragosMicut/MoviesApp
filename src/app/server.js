ca02e9e4d54722e1f23387255e91bf10e6557c69
DragosMicut <dragos_micut@yahoo.com>
2022-09-26 14:37:08 +0300

    deploy v3

--- src/app/server.js
+++ src/app/server.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static(__dirname+'/dist/watch-bucket'));
 
 app.get('/*',(req,res)=>{
-    res.sendFile(__dirname+'/dist/watch-bucket/index.html')
+    res.sendFile(__dirname+'/dist/movies-list/index.html')
 })
 
 app.listen(process.env.port ||8080)
